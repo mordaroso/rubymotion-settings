87d9e83e50c8e1713a135e03a6533b5996ac5fd4
mordaroso <mordaroso@gmail.com>
2012-05-24 09:23:22 +0200

    refactor setDefaultSettings to use a MacRuby Hash

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -21,11 +21,7 @@ class AppDelegate
       settingsDictionary = NSDictionary.dictionaryWithContentsOfFile plistFile
       preferencesArray   = settingsDictionary.objectForKey "PreferenceSpecifiers"
 
-      textEntry_Key = nil
-      readOnly_Key = nil
-      toogle_Key = nil
-      slider_Key = nil
-      colors_Key = nil
+      appPrerfs = {}
 
       preferencesArray.each do |item|
         # Get the key of the item.
@@ -33,25 +29,9 @@ class AppDelegate
 
         # Get the default value specified in the plist file.
         defaultValue = item.objectForKey "DefaultValue"
-        p keyValue
-        case keyValue
-        when "textEntry_key"
-          textEntry_Key = defaultValue
-        when "readOnly_key"
-          readOnly_Key = defaultValue
-        when "toogle_key"
-          toogle_Key = defaultValue
-        when "slider_key"
-          slider_Key = defaultValue
-        when "colors_key"
-          colors_Key = defaultValue
-        end
+
+        appPrerfs[keyValue] = defaultValue unless keyValue.nil? || defaultValue.nil?
       end
-
-      # Now that we have all the default values.
-      # We will create it here.
-      appPrerfs = NSDictionary.dictionaryWithObjects [textEntry_Key, readOnly_Key, toogle_Key, slider_Key, colors_Key], forKeys: ["textEntry_key", "readOnly_key", "toogle_key", "slider_key", "colors_key"]
-
 
       NSUserDefaults.standardUserDefaults.registerDefaults appPrerfs
       NSUserDefaults.standardUserDefaults.synchronize
